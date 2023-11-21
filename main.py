857426c78c7a5af5e44547ad58beb873955b1c8b
Yixuan Li <elainelyx114@gmail.com>
2023-11-20 19:23:58 -0500

    add information

--- main.py
+++ main.py
@@ -17,57 +17,58 @@ def hello_world():
 
 @app.get("/product_async")
 async def async_call():
-    IDList = ["1"]
+    IDList = ["1","2"]
     result = await service.getProductAsync(IDList)
     return JSONResponse(content={"message": "This is from async get.","products": result})
 
 @app.get("/product_sync")
 async def async_call():
-    IDList = ["1"]
+    IDList = ["1","2"]
     result = await service.getProductSync(IDList)
     return JSONResponse(content={"message": "This is from sync get.","products": result})
 
 @app.get("/customer_async")
 async def async_call():
-    IDList = ["1"]
+    IDList = list(range(1,6))
+    IDList = map(str,IDList)
     result = await service.getCustomerAsync(IDList)
-    return JSONResponse(content={"customers": result})
+    return JSONResponse(content={"message": "This is from async get.","customers": result})
 
 @app.get("/customer_sync")
 async def async_call():
-    IDList = ["1","2"]
+    IDList = ["1","2","3"]
     result = await service.getCustomerSync(IDList)
-    return JSONResponse(content={"customers": result})
+    return JSONResponse(content={"message": "This is from sync get.","customers": result})
 
 @app.get("/staff_async")
 async def async_call():
-    IDList = ["1"]
+    IDList = ["1","2","3","4","5"]
     result = await service.getStaffAsync(IDList)
-    return JSONResponse(content={"staffs": result})
+    return JSONResponse(content={"message": "This is from sync get.","message": "This is from async get.","staffs": result})
 
 @app.get("/staff_sync")
 async def async_call():
     IDList = ["1","2"]
     result = await service.getStaffSync(IDList)
-    return JSONResponse(content={"staffs": result})
+    return JSONResponse(content={"message": "This is from sync get.","staffs": result})
 
 @app.get("/order_async")
 async def async_call():
-    IDList = ["1","2"]
+    IDList = ["1","2","3","4","5"]
     result = await service.getOrderAsync(IDList)
-    return JSONResponse(content={"orders": result})
+    return JSONResponse(content={"message": "This is from async get.","orders": result})
 
 @app.get("/order_sync")
 async def async_call():
-    IDList = ["1","2"]
+    IDList = ["1","2","3","4","5"]
     result = await service.getOrderSync(IDList)
-    return JSONResponse(content={"orders": result})
+    return JSONResponse(content={"message": "This is from sync get.","orders": result})
 
 @app.get("/meeting_sync")
 async def async_call():
     IDList = ["1","2"]
     result = await service.getOrderSync(IDList)
-    return JSONResponse(content={"meetings": result})
+    return JSONResponse(content={"message": "This is from sync get.","meetings": result})
 
 
 if __name__ == "__main__":
